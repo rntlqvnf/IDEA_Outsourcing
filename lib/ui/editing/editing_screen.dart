6361df40819ee2fe89f00a54fde6aedb4ca819dc
hajae <rntlqvnf@naver.com>
2020-09-10 21:33:50 +0900

    <Fix> : Fix character plugin not found bug

--- lib/ui/editing/editing_screen.dart
+++ lib/ui/editing/editing_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class EditingScreen extends StatelessWidget {
   const EditingScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final image = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+          leading: IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: () => Navigator.of(context).pop(),
+      )),
+      body: Image.memory(
+        image,
+        fit: BoxFit.cover,
+      ),
+    );
   }
 }
